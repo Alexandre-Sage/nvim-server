71338ae4e57032a998d5f1f30a2499b7a4405dc1
Your Name <you@example.com>
2025-08-20 09:54:13 +0200

    install

---
+++ install.sh
@@ -0,0 +1,8 @@
+#UBUNTU
+apt-get install node npm cmake gcc fd-find fzf ripgrep curl -y
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
