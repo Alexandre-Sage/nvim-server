8c8e4ca217b00c80f5d1a578698b088b6aaecbb0
Alexandre Sage (ASE) <alexandre.sage@adsoftware.fr>
2025-08-25 11:07:42 +0200

    ok

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+
+ln -s /opt/nvim-linux-x86_64/bin/nvim /bin/nvim
